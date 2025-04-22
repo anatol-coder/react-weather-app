fa94d2e82b3638fee66089faa63cda439d2e28ba
anatol-coder <ankawysota@gmail.com>
2025-04-22 16:26:34 +0100

    Links to GitHub repo and Netlify added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Hello from React</header>
+      <footer>
+        This project was coded by Anna Wysota and is{" "}
+        <a
+          href="https://github.com/anatol-coder/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://silver-gumption-c38209.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
